b06070f834ff7af22689f458e50efa16d6cda4b0
keivan2 <keivan.2karimi@gmail.com>
2023-09-24 17:42:17 +0200

    add abort_all_positions function

--- program/main.py
+++ program/main.py
@@ -1,11 +1,22 @@
+from constants import ABORT_ALL_POSITIONS
 from func_connections import connect_dydx
+from func_private import abort_all_positions
 
 if __name__ == "__main__":
     
     # Connect to client
     try:
+        print("Connecting to Client...")
         client = connect_dydx()
     except Exception as e:
-        print(e)
         print("Error connecting to client: ", e)
         exit(1)
+
+    # Abort all open positions
+    if ABORT_ALL_POSITIONS:
+        try:
+            print("Closing all positions...")
+            close_orders = abort_all_positions(client)
+        except Exception as e:
+            print("Error closing all positions to cleint: ", e)
+            exit(1)
