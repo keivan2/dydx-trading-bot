60c39bb452663318fcecf293c969fb644c52deb5
keivan2 <keivan.2karimi@gmail.com>
2023-09-25 19:36:53 +0200

    Add find_cointegrated

--- program/main.py
+++ program/main.py
@@ -1,6 +1,7 @@
-from constants import ABORT_ALL_POSITIONS
+from constants import ABORT_ALL_POSITIONS, FIND_COINTEGRATED
 from func_connections import connect_dydx
 from func_private import abort_all_positions
+from func_public import construct_market_prices
 
 if __name__ == "__main__":
     # Connect to client
@@ -19,3 +20,15 @@ if __name__ == "__main__":
         except Exception as e:
             print("Error closing all positions to cleint: ", e)
             exit(1)
+            
+    # Find Cointegrated Pairs
+    if FIND_COINTEGRATED:
+        
+        # Construct Market Prices
+        try:
+            print("Fetching market prices, please allow 3 mins...")
+            df_market_prices = construct_market_prices(client)
+        except Exception as e:
+            print("Error constructing market prices: ", e)
+            exit(1)
+        
